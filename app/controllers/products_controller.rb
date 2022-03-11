6e16891336835579118d9985be927a5bce654b13
Sam Sharff <samanthaleec@gmail.com>
2022-03-11 13:40:37 -0500

    adds RESTFUL convention, show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,12 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+  
+  def show
+    product_id = params[:id]
+    products = Product.find_by(id: product_id)
+    render json: products.as_json
+  end
 
   # def single_product
   #   input_product = params["id"]
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
 get "/products", controller: "products", action: "index"
 
+get "/products/:id", controller: "products", action: "show"
+
 end

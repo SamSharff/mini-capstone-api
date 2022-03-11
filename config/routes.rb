5a68c014d2df750c6eed20a7adcb10944abc3f8c
Sam Sharff <samanthaleec@gmail.com>
2022-03-11 12:56:44 -0500

    adds RESTFUL convention, index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-get "/available_products", controller: "products", action: "available_products"
+# get "/available_products", controller: "products", action: "available_products"
 
-get "/single_product", controller: "products", action: "single_product"
+# get "/single_product", controller: "products", action: "single_product"
+
+get "/products", controller: "products", action: "index"
 
 end

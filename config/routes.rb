0c53bd1c17372dc667b720346d8572381027ed2c
Sam Sharff <samanthaleec@gmail.com>
2022-03-22 14:49:31 -0400

    adds authentication work (untested, needs work); adds authorization exercise step 1 and 2)

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ delete "/products/:id", controller: "products", action: "destroy"
 
 post "/users" => "users#create" #sign up
 post "/sessions" => "sessions#create" #sessions
+
 post "/orders" => "orders#create" #creates order
 get "/orders/:id" => "orders#show" #shows order
+get "/orders/" => "orders#index" #shows order
 
 end
